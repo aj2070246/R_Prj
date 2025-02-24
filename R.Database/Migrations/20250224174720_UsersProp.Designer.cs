3d2b991644ebd7402d267fb37263ed30ee48abdf
amirx128 <AmirX128@outlook.com>
2025-02-24 21:18:24 +0330

    update user table with new props

--- R.Database/Migrations/20250224174720_UsersProp.Designer.cs
+++ R.Database/Migrations/20250224174720_UsersProp.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using R.Database;
 
@@ -11,9 +12,11 @@ using R.Database;
 namespace R.Database.Migrations
 {
     [DbContext(typeof(RDbContext))]
-    partial class RDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250224174720_UsersProp")]
+    partial class UsersProp
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ R.Database/Migrations/20250224174720_UsersProp.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace R.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class UsersProp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CheildCount",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "FirstCheildAge",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Ghad",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "RangePoost",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "TipNUmber",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Vazn",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ZibaeeNumber",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CheildCount",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "FirstCheildAge",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Ghad",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "RangePoost",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TipNUmber",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Vazn",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ZibaeeNumber",
+                table: "Users");
+        }
+    }
+}
